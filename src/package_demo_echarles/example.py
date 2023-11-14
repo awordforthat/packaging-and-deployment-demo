4abf3f7eaefb45e19868384dbaaf55b9eb1c7e40
Emily Charles <emily.w.charles@gmail.com>
2023-11-14 18:58:51 -0500

    initial commit

---
+++ src/package_demo_echarles/example.py
@@ -0,0 +1,10 @@
+"""A module to demonstrate source distributions."""
+
+
+def say_hello(name):
+    """Says hello!"""
+    print(f"Hello, {name}!")
+
+
+def some_useful_algorithm():
+    """An algorithm that you don't want to write yourself"""
